dd876d40ba90815a6909f9a8c5ec470a29a24120
Robin Kaiser <web@r-k.mx>
2023-10-08 04:50:02 +0200

    add ctor and hardcopy method

--- ActionlistGenerator/Preset.cs
+++ ActionlistGenerator/Preset.cs
@@ -8,5 +8,17 @@ namespace ActionlistGenerator
         public string Name { get; set; }
         public string Description { get; set; }
         public List<XElement> Elements { get; set; }
+
+        public Preset(string name, string desc, List<XElement> elems)
+        {
+            this.Name = name;
+            this.Description = desc;
+            this.Elements = elems;
+        }
+
+        public Preset HardCopy()
+        {
+            return new Preset(this.Name, this.Description, this.Elements);
+        }
     }
 }
